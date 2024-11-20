c6dd8fcfc314b460cfb7ff41401b5cafaad496db
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-11-20 20:03:54 +0100

    Refactor | Ejercicio 4

---
+++ Ejercicio4/Ejercicio4.c
@@ -0,0 +1,22 @@
+//
+// Created by pinchiguillo on 20/11/2024.
+//
+
+#include "Ejercicio4.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Ejercicio1/Ejercicio1.h"
+
+void Ejercicio4() {
+
+    typedef struct Estudiante Estudiante;
+
+    Estudiante estudiante3;
+    strcpy(estudiante3.nombre, "Pedro");
+    estudiante3.edad = 25;
+    estudiante3.promedio = 9.7;
+
+}
---
+++ Ejercicio4/Ejercicio4.h
@@ -0,0 +1,10 @@
+//
+// Created by pinchiguillo on 20/11/2024.
+//
+
+#ifndef EJERCICIO4_H
+#define EJERCICIO4_H
+
+void Ejercicio4();
+
+#endif //EJERCICIO4_H
